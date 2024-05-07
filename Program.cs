58133e9cc28fa7be20756bfbb84e0774f5f5ff27
Şule Merve Atalay <93016886+suleatalay@users.noreply.github.com>
2024-05-07 13:41:53 +0300

    s cookie management

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+{
+    option.LoginPath = "/Home/Login/";
+    option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+});
 
 var app = builder.Build();
 
